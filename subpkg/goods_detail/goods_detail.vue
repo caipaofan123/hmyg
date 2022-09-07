e0bc95c553daae247d98c41c6d6de1cba0a41d97
caipaofan <954698621@qq.com>
2022-09-07 09:00:46 +0800

    done index

---
+++ subpkg/goods_detail/goods_detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    商品详情页23131421431341
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
